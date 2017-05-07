b4ce1ab7d68938c237a4b6353be829f331ef8a0a
Jun <jason830605@gmail.com>
2017-05-08 00:58:28 +0800

    finish L1

--- hw4.py
+++ hw4.py
@@ -3,31 +3,43 @@ import sys
 filenm = sys.argv[1]
 sup = 0.3
 confidence = 0.3
-
+Tx = {}
 C1 = {}
+L1 = {}
+C2 = {}
+L2 = {}
 #print sys.argv[1], sys.argv[2]
+i=1
+for line in open(filenm).readlines():
+	lis=line.split()
+	Tx.update({i:lis})
+	i = i+1
 
-f = open(filenm)
-text = f.read().split()
-f.close()
 num_lines = sum(1 for line in open(filenm))
 sup_count = num_lines * sup
+
 print "sup count:",sup_count
+text = (open(filenm)).read().split()
 
 for item in text:
 	C1.update({item:C1.get(item,0)})
 	C1[item] = C1.get(item,0)+1
+indL1=1 #L1 index
 for (item,count) in C1.items():
-	print item,count
+	#print item,count
 	if count < sup_count :
 		del C1[item]
-		#print "del--------------------------------------------------------------"
-		#print item,count
-print "after del"
-for (item,count) in C1.items():
-	print item,count
+	else:
+		L1.update({indL1:item})
+		indL1 = indL1+1
 
-f = open(filenm)
+print "L1: ",L1
+
+
+
+#print "after del"
+#for (item,count) in C1.items():
+#	print item,count
 
 
 
