fc352fab1ec744d5f42659003fa5eb086d45caba
Xking8 <jason830605@gamil.com>
2017-05-08 23:05:02 +0800

    finish L2

--- hw4.py
+++ hw4.py
@@ -9,14 +9,15 @@ L1 = {}
 C2 = {}
 L2 = {}
 #print sys.argv[1], sys.argv[2]
-i=1
+i=0
 for line in open(filenm).readlines():
 	lis=line.split()
 	Tx.update({i:lis})
 	i = i+1
 
-num_lines = sum(1 for line in open(filenm))
-sup_count = num_lines * sup
+num_lines = i
+#num_lines = sum(1 for line in open(filenm))
+sup_count = int (round(num_lines * sup))
 
 print "sup count:",sup_count
 text = (open(filenm)).read().split()
@@ -24,7 +25,7 @@ text = (open(filenm)).read().split()
 for item in text:
 	C1.update({item:C1.get(item,0)})
 	C1[item] = C1.get(item,0)+1
-indL1=1 #L1 index
+indL1=0 #L1 index
 for (item,count) in C1.items():
 	#print item,count
 	if count < sup_count :
@@ -33,7 +34,46 @@ for (item,count) in C1.items():
 		L1.update({indL1:item})
 		indL1 = indL1+1
 
-print "L1: ",L1
+print "L1: ",L1,"indL1:",indL1
+
+#start C2 L2
+indC2 = 0
+for i in xrange(indL1-1):
+	for j in xrange(i+1,indL1):
+		#print i,",",j
+		C2.update( { indC2 : [L1[i],L1[j],0] } )
+		indC2 += 1
+		#print L1[i], L1[j]
+print "h2" 
+for (C2d,[p1,p2,count]) in C2.items():
+	print C2d,p1,p2,count
+	for Tranc,Tlist in Tx.items():
+		#print "Tlist:",Tlist
+		p1ck = False
+		p2ck = False
+		for obj in Tlist:
+			if obj == p1 :
+				 p1ck = True;
+			if obj == p2 :
+				 p2ck = True;
+		if p1ck and p2ck:
+			count += 1
+			C2[C2d][2]+=1
+print "C2:+++",C2			
+C3List = []
+for (C2d,[p1,p2,count]) in C2.items():
+	if count < sup_count:
+		del C2[C2d]
+	else:
+		if p1 not in C3List:
+			C3List.append(p1)
+		if p2 not in C3List:
+			C3List.append(p2)
+		
+print "L2:***",C2
+print "C3List",C3List,len(C3List)
+
+#start C3 L3
 
 
 
